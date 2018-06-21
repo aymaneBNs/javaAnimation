24ddf12f28959a6c6a0f05f4ad7c53b505822c18
aimene <aymanemoc1@gmail.com>
2018-06-21 13:10:29 +0200

    add nes constructer with z

--- src/application/Point.java
+++ src/application/Point.java
@@ -7,13 +7,19 @@ public class Point {
 	
 	private int x;
 	private int y;
-	
+	private int z;
 	
 	
 	
 	public Point(int x,int y) {
 		
 		this.x=x;  this.y=y;
+	}
+	
+	
+	public Point(int x,int y,int z) {
+		
+		this.x=x;  this.y=y;   this.z=z;
 	}
 
 	
